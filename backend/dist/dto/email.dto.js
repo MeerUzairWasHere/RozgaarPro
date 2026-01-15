496787bf5769ea66cb6aaa4bfd0699620e052639
Meer Uzair <meer.uxair007@gmail.com>
2026-01-15 22:36:29 +0530

    Remove compiled backend distribution files
    Deleted all files in backend/dist, including controllers, services, repositories, middlewares, DTOs, interfaces, guards, filters, and utility modules. This change likely reflects a cleanup of compiled output or a transition in build/deployment strategy.

--- backend/dist/dto/email.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmailTypeDto = void 0;
-var EmailTypeDto;
-(function (EmailTypeDto) {
-    EmailTypeDto["VERIFICATION"] = "verification";
-    EmailTypeDto["WELCOME"] = "welcome";
-    EmailTypeDto["RESET_PASSWORD"] = "reset-password";
-})(EmailTypeDto || (exports.EmailTypeDto = EmailTypeDto = {}));
--- backend/dist/utils/createHash.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hashString = void 0;
-const crypto_1 = require("crypto");
-const hashString = (string) => {
-    return (0, crypto_1.createHash)("md5").update(string).digest("hex");
-};
-exports.hashString = hashString;
