40d230e744b07b51166e59d1ae174fe7de3deae5
Meer Uzair <meer.uxair007@gmail.com>
2026-01-22 09:40:52 +0530

    Update metro.config.js

--- native/metro.config.js
+++ native/metro.config.js
@@ -1,5 +1,5 @@
 const { withNativeWind } = require("nativewind/metro");
-const { getDefaultConfig } = require("tailwind-merge");
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
